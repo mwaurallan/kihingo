01ebf76084797c6292e8c6f162f8d6df882edaf3
mwaurallan <allanmwaura2013@gmail.com>
2024-03-25 18:07:33 +0300

    Add existing project files to Git

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.home')
+@section('title', config('app.name', 'Wakwetu'))
+
+@section('content')
+    <style type="text/css">
+        .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+                margin-top: 10%;
+            }
+        .title {
+                font-size: 84px;
+            }
+        .tagline {
+                font-size:25px;
+                font-weight: 300;
+                text-align: center;
+            }
+    </style>
+    <div class="title flex-center" style="font-weight: 600 !important;">
+        {{ config('app.name', 'wakwetu') }}
+    </div>
+    <p class="tagline">
+        {{ env('APP_TITLE', '') }}
+    </p>
+
+@endsection
+            
